dc1678718191e51672aaff1325ed1ecdbd404833
durmaz54 <55615634+durmaz54@users.noreply.github.com>
2023-04-11 15:21:22 +0000

    s

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import can
 import time
 
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=250000)
-
+motorLeft = 0
 while True:
     time.sleep(1)
+    motorLeft+=0.1
     message = can.Message(arbitration_id=0x17, data=bytes(str(motorLeft), 'utf-8'), is_extended_id=False)
     bus.send(message, timeout= 0.1)
