97d531523d302b8d2376879ba22715d86add01e6
durmaz54 <55615634+durmaz54@users.noreply.github.com>
2023-04-15 07:20:36 +0000

    1

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import can
 import time
-
+import os
+"""""
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=250000)
 motorLeft = 0
 while True:
@@ -9,4 +10,10 @@ while True:
     message = can.Message(arbitration_id=0x17, data=bytes(str(motorLeft), 'utf-8'), is_extended_id=False)
     bus.send(message, timeout= 0.1)
     print("------------------------")
-    print(motorLeft)
+    print(motorLeft)""""
+
+a = 0
+
+while True:
+    time.sleep(1)
+    os.system("cansend can0 017#{}".format(str(1234)))
