934cf9ec339cebb01b654e150316f5d29001e8f9
durmaz54 <55615634+durmaz54@users.noreply.github.com>
2023-04-15 08:36:26 +0000

    32

--- test.py
+++ test.py
@@ -4,10 +4,19 @@ import os
 
 
 a = 0
+def send(data):
+    x = str(data)
+    if(len(x)<8):
+        x = x + "0"*(8-len(x))
+    elif (len(x)>8):
+        x = x[0:8]
+    x = bytes(str(x),'utf-8')
+    x = binascii.hexlify(x)
+    os.system("cansend can0 017#{}".format(str(x, 'utf-8')))
+
 
 while True:
     time.sleep(1)
-    txt = bytes(str(a), 'utf-8')
-    print(txt)
-    os.system("cansend can0 017#{}".format(txt))
+    send(a)
     a+=0.3
+
