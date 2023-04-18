af945f2458874a843d6cb8336088e52e17927fa3
durmaz54 <55615634+durmaz54@users.noreply.github.com>
2023-04-18 09:15:03 +0000

    sa

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ import threading
 import time
 import numpy as np
 
-def send():
+bus = can.interface.Bus(bustype='socketcan', channel='can0')
 
-    pass
+
+def send(m1,m2):
+    m1 = int(float1 * 100)
+    m2 = int(float2 * 100)
+    data1 = struct.pack("i",m1)
+    data2 = struct.pack("i",m2)
+    data = data1+data2
+    print(data)
+    msg = can.Message(arbitration_id=0x17, data=data)
+    print("f1 = {} f2={}".format(str(m1),str(m2)))
+    bus.send(msg)
+    print(msg)
+    
 
 def canLoop():
     while True:
         agv.read2STM()
         
 def loop():
-    bus = can.interface.Bus(bustype='socketcan', channel='can0')
 
-    float1 = -1.09
-    float2 = -1.15
+    float1 = 0
+    float2 = 0
 
     while True:
         float1 += 0.1
-        float2 += 0.2 
-        a = int(float1 * 100)
-        b = int(float2 * 100)
-        data1 = struct.pack("i",a)
-        data2 = struct.pack("i",b)
-        data = data1+data2
-        print(data)
-        msg = can.Message(arbitration_id=0x17, data=data)
-        print("f1 = {} f2={}".format(str(a),str(b)))
-        bus.send(msg)
-        print(msg)
+        float2 -= 0.1 
+        send(float1,float2)
         time.sleep(1)
 
 #t1 = threading.Thread(target=canLoop)
