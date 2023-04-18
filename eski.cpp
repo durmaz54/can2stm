554d152f9a0b237760c0192193d37773d77a1a76
durmaz54 <abdulsametdurmaz54@gmail.com>
2023-04-18 16:46:40 +0300

    s

--- eski.cpp
+++ eski.cpp
@@ -1,5 +1,7 @@
 #include <ncurses.h>
+#include <iostream>
 
+using namespace std;
 int main() {
     // Initialize ncurses library
     initscr();
@@ -14,18 +16,23 @@ int main() {
         
         // Check if any arrow key is pressed
         if (ch == KEY_UP) {
+            cout << "up" << endl;
             // Up arrow key is pressed
             // do something
         } else if (ch == KEY_DOWN) {
+            cout << "down" << endl;
             // Down arrow key is pressed
             // do something
         } else if (ch == KEY_LEFT) {
+            cout << "left" << endl;
             // Left arrow key is pressed
             // do something
         } else if (ch == KEY_RIGHT) {
+            cout << "right" << endl;
             // Right arrow key is pressed
             // do something
         } else if (ch == 'q' || ch == 'Q') {
+            cout << "q" << endl;
             // Q key is pressed
             break;
         } else {
